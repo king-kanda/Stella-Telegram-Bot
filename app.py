8cdf31991db7014a555293e6a354425cb74a1e18
king-kand <stevenokanda@gmail.com>
2023-04-26 00:03:24 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+from telegram.ext.updater import Updater
+from telegram.update import Update
+from telegram.ext.callbackcontext import CallbackContext
+from telegram.ext.commandhandler import CommandHandler
+from telegram.ext.messagehandler import MessageHandler
+from telegram.ext.filters import Filters
+
+# initialize API token - Telegram
+telegram_token = '5630483237:AAHbJxPjShtrh0KXEt8BPhMyLGQDVJyVpuk'
+
+# create an Updater object with the token
+updater = Updater(token=telegram_token, use_context=True)
+dispatcher = updater.dispatcher
+
+def start(update, context):
+  update.message.reply_text('Hello! Welcome to anandsdata BOT! Enjoy the resource!')    
+  update.message.reply_text('Type /help for the content to display!')
+  update.message.reply_text('Happy Learning!')
+  
+def help(update, context):
+  update.message.reply_text(
+      """
+      /start -> Welcome to the anandsdata BOT!
+      /help -> This particular message
+      /position -> Current position in the roster
+      /contact -> contact information
+      """)
+  
+def contact(update, context):
+  update.message.reply_text('Blog link: https://okandasteven.com')
+  
+
+dispatcher = updater.dispatcher
+updater.dispatcher.add_handler(CommandHandler('start', start))
+updater.dispatcher.add_handler(CommandHandler('help', help))
+
+updater.dispatcher.add_handler(CommandHandler('contact',contact))
+updater.start_polling()  # Start the bot
+updater.idle() # Wait for the script to be stopped; this will stop the bot
---
+++ main.py
@@ -0,0 +1,41 @@
+from telegram import Update
+from telegram.ext import Updater, CommandHandler
+import sys
+
+# Initialize API token - Telegram
+telegram_token = '5630483237:AAHbJxPjShtrh0KXEt8BPhMyLGQDVJyVpuk'
+
+# Creating objects to perform a task
+updater = Updater(telegram_token , use_context=True)
+dispatcher = updater.dispatcher
+
+# Define function to handle /start command
+def start(update, context):
+    update.message.reply_text('Hello! Welcome to the Stella BOT! Enjoy the resource!')
+    update.message.reply_text('Type /help for the content to display!')
+    update.message.reply_text('Happy Learning!')
+
+# Define function to handle /help command
+def help(update, context):
+    update.message.reply_text(
+        """
+        /start -> Welcome to the Stella BOT!
+        /help -> This particular message
+        /position -> Current position in the roster
+        /contact -> contact information
+        """)
+
+# Define function to handle /contact command
+def contact(update, context):
+    update.message.reply_text('Blog link: https://okandasteven.com')
+
+# Add handlers for commands
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(CommandHandler('help', help))
+dispatcher.add_handler(CommandHandler('contact', contact))
+
+# Start the bot
+updater.start_polling()
+
+# Run the bot until you press Ctrl-C
+updater.idle()
