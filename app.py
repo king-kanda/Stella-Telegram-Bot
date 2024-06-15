861f71b9a69568c7d43250465c32d20483e767b8
kingkanda <stevenokanda@gmail.com>
2024-06-16 00:42:07 +0300

    updating env

--- app.py
+++ app.py
@@ -1,39 +1,41 @@
-from telegram.ext.updater import Updater
-from telegram.update import Update
-from telegram.ext.callbackcontext import CallbackContext
-from telegram.ext.commandhandler import CommandHandler
-from telegram.ext.messagehandler import MessageHandler
-from telegram.ext.filters import Filters
+import os
+from telegram.ext import Application, CommandHandler
+from dotenv import load_dotenv
 
-# initialize API token - Telegram
-telegram_token = '3237:yVpuk'
+# Load environment variables from .env file
+load_dotenv()
 
-# create an Updater object with the token
-updater = Updater(token=telegram_token, use_context=True)
-dispatcher = updater.dispatcher
+# Get the token from the environment variable
+telegram_token = os.getenv('token')
+
+if not telegram_token:
+    raise ValueError("No TELEGRAM_TOKEN found in environment variables")
+
+# Create an Application object with the token
+application = Application.builder().token(telegram_token).build()
 
 def start(update, context):
-  update.message.reply_text('Hello! Welcome to anandsdata BOT! Enjoy the resource!')    
-  update.message.reply_text('Type /help for the content to display!')
-  update.message.reply_text('Happy Learning!')
-  
+    update.message.reply_text('Hello! Welcome to anandsdata BOT! Enjoy the resource!')
+    update.message.reply_text('Type /help for the content to display!')
+    update.message.reply_text('Happy Learning!')
+
 def help(update, context):
-  update.message.reply_text(
-      """
-      /start -> Welcome to the anandsdata BOT!
-      /help -> This particular message
-      /position -> Current position in the roster
-      /contact -> contact information
-      """)
-  
+    update.message.reply_text(
+        """
+        /start -> Welcome to the anandsdata BOT!
+        /help -> This particular message
+        /position -> Current position in the roster
+        /contact -> contact information
+        """
+    )
+
 def contact(update, context):
-  update.message.reply_text('Blog link: https://okandasteven.com')
-  
+    update.message.reply_text('Blog link: https://okandasteven.com')
 
-dispatcher = updater.dispatcher
-updater.dispatcher.add_handler(CommandHandler('start', start))
-updater.dispatcher.add_handler(CommandHandler('help', help))
+# Add command handlers to the application
+application.add_handler(CommandHandler('start', start))
+application.add_handler(CommandHandler('help', help))
+application.add_handler(CommandHandler('contact', contact))
 
-updater.dispatcher.add_handler(CommandHandler('contact',contact))
-updater.start_polling()  # Start the bot
-updater.idle() # Wait for the script to be stopped; this will stop the bot
+# Start the bot
+application.run_polling()
